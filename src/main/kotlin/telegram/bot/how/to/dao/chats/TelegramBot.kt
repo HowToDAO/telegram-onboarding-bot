6504322f5f9932e3e23d25f01c1ea3a28a46b5f5
petrovMA <petrovma92@gmail.com>
2022-12-18 01:30:36 +0800

    added FAQ answers

--- src/main/kotlin/telegram/bot/how/to/dao/chats/TelegramBot.kt
+++ src/main/kotlin/telegram/bot/how/to/dao/chats/TelegramBot.kt
@@ -1,21 +1,28 @@
 package telegram.bot.how.to.dao.chats
 
+import com.google.gson.Gson
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText
 import org.telegram.telegrambots.meta.api.objects.Update
-import telegram.bot.how.to.dao.database.data.entity.User
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+import telegram.bot.how.to.dao.database.dto.FrequentlyAskedQuestionsDTO
 import telegram.bot.how.to.dao.database.service.UserService
-import java.sql.Timestamp
+import java.io.File
+
 
 class TelegramBot(
     private val botUsername: String,
     private val botToken: String,
+    fileName: String,
     override val userService: UserService
 ) : Communicator, TelegramLongPollingBot() {
 
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
+    private val answers = read(fileName)
 
     override fun onUpdateReceived(update: Update) {
         log.debug(
@@ -28,18 +35,41 @@ class TelegramBot(
                     "\nSticker: " + update.message?.sticker
         )
 
-        val user = User(
-            id = null,
-            userChatId = update.message.chatId.toString(),
-            firstName = update.message?.from?.firstName,
-            lastName = update.message?.from?.lastName,
-            userName = update.message?.from?.userName,
-            createDate = Timestamp(System.currentTimeMillis())
-        )
+        if (update.hasCallbackQuery()) {
+            // Set variables
+            val callData = update.callbackQuery.data;
+            val messageId = update.callbackQuery.message.messageId
+            val chatId = update.callbackQuery.message.chatId
 
-        update.message?.text
-            ?.let { onUpdate(it, user) }
-            ?.run { sendMessage(this, update.message.chatId) }
+            val answer = findAnswer(callData, answers)
+
+            sendMessageWithButtons(
+                messageText = answer?.text ?: " -- text not found -- ",
+                chatId = chatId,
+                messageId = messageId,
+                answer = answer ?: answers
+            )
+        } else {
+            sendMessageWithButtons(
+                messageText = answers.text,
+                chatId = update.message.chatId,
+                answer = answers
+            )
+        }
+
+
+//        val user = User(
+//            id = null,
+//            userChatId = update.message.chatId.toString(),
+//            firstName = update.message?.from?.firstName,
+//            lastName = update.message?.from?.lastName,
+//            userName = update.message?.from?.userName,
+//            createDate = Timestamp(System.currentTimeMillis())
+//        )
+//
+//        update.message?.text
+//            ?.let { onUpdate(it, user) }
+//            ?.run { sendMessage(this, update.message.chatId) }
     }
 
     private fun sendMessage(messageText: String, chatId: Long): Unit = try {
@@ -51,6 +81,93 @@ class TelegramBot(
         Unit
     } catch (e: Exception) {
         log.error(e.message, e)
+    }
+
+    private fun sendMessageWithButtons(
+        messageText: String,
+        chatId: Long,
+        answer: FrequentlyAskedQuestionsDTO,
+        messageId: Int? = null
+    ): Unit = try {
+        messageId?.let {
+            EditMessageText().also {
+                it.chatId = chatId.toString()
+                it.messageId = messageId
+                it.text = messageText
+
+                val rowsInline: MutableList<List<InlineKeyboardButton>> = if (answer.list.isNullOrEmpty().not())
+                    answer.list!!.map {
+                        ArrayList<InlineKeyboardButton>().apply {
+                            addAll(listOf(
+                                InlineKeyboardButton().also { btn ->
+                                    btn.text = it.button ?: " -- text for button not found -- "
+                                    btn.callbackData = it.code
+                                }
+                            ))
+                        }
+                    }.toMutableList()
+                else ArrayList()
+
+                rowsInline.let { rows -> it.replyMarkup = InlineKeyboardMarkup().apply { keyboard = rows } }
+
+                execute(it)
+            }
+        } ?: SendMessage().also {
+            it.chatId = chatId.toString()
+            it.text = messageText
+
+            val rowsInline: MutableList<List<InlineKeyboardButton>> = if (answer.list.isNullOrEmpty().not())
+                answer.list!!.map {
+                    ArrayList<InlineKeyboardButton>().apply {
+                        addAll(listOf(
+                            InlineKeyboardButton().also { btn ->
+                                btn.text = it.button ?: " -- text for button not found -- "
+                                btn.callbackData = it.code
+                            }
+                        ))
+                    }
+                }.toMutableList()
+            else ArrayList()
+
+            rowsInline.let { rows -> it.replyMarkup = InlineKeyboardMarkup().apply { keyboard = rows } }
+
+            execute(it)
+        }
+        Unit
+    } catch (e: Exception) {
+        log.error(e.message, e)
+    }
+
+    private fun read(fileName: String): FrequentlyAskedQuestionsDTO {
+        val jsonString = File(fileName).inputStream().readBytes().toString(Charsets.UTF_8)
+        val answers = Gson().fromJson(jsonString, FrequentlyAskedQuestionsDTO::class.java)
+
+        val codes = mutableSetOf<String>()
+
+        fun validate(answers: FrequentlyAskedQuestionsDTO) {
+            answers.list?.forEach {
+                it.code?.let { code ->
+                    if (codes.contains(code)) {
+                        throw RuntimeException("Code '$code' is duplicated in '$fileName'")
+                    } else {
+                        codes.add(code)
+                    }
+                }
+                validate(it)
+            }
+        }
+
+        validate(answers)
+
+        return answers
+    }
+
+    private fun findAnswer(code: String, answers: FrequentlyAskedQuestionsDTO): FrequentlyAskedQuestionsDTO? {
+        answers.list?.forEach { answer ->
+            if (answer.code == code) return answer
+            findAnswer(code, answer)?.let { return it }
+        }
+        return null
     }
 
     override fun getBotUsername(): String = botUsername
